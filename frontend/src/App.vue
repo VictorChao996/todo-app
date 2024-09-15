d4c7c1469bcfa8e8ce490e84e86658f86e49d060
VictorChao <96094victor@gmail.com>
2024-09-15 14:25:31 +0800

    feat: add vue-route logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,12 @@
 <script setup>
-	import { ref } from "vue";
-	import axios from "axios";
-	import Todo from "./component/Todo.vue";
-
-	const todos = ref([]);
-	const new_todo_text = ref("");
-	init();
-
-	/**
-	 * Fetch data from server
-	 */
-	function init() {
-		//get data from localStorage first
-		try {
-			const localStorageTodos = localStorage.getItem("todos");
-			if (localStorageTodos) {
-				todos.value = JSON.parse(localStorage.getItem("todos"));
-			}
-		} catch (e) {
-			console.log(e);
-		}
-
-		//fetch data from backend
-		axios.get("http://localhost:3000/api/todos").then((response) => {
-			console.log(response.data);
-			todos.value = response.data;
-		});
-	}
-
-	function add() {
-		todos.value.push(new_todo_text.value);
-		new_todo_text.value = "";
-	}
-
-	function clearData() {
-		todos.value = [];
-	}
+	import { RouterLink, RouterView, useRoute, useRouter } from "vue-router";
 </script>
 
 <template>
-	<!-- <button @click="fetchData">fetch data from server</button> -->
-	<button @click="clearData">clear Data</button>
-	<input
-		v-model="new_todo_text"
-		type="text"
-		placeholder="enter todo title"
-		@keyup.enter="add"
-	/>
-	<button @click="add">add todo</button>
-	<Todo :todos="todos" />
+	<nav>
+		<RouterLink to="/">Home Page</RouterLink>
+		<RouterLink to="/about">About Page</RouterLink>
+	</nav>
+	<div>----</div>
+	<RouterView />
 </template>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from "vue";
+import { createMemoryHistory, createRouter } from "vue-router";
+
 import App from "./App.vue";
+import AboutView from "./view/AboutView.vue";
+import HomeView from "./view/HomeView.vue";
 
 const app = createApp(App);
+const routes = [
+	{ path: "/", component: HomeView },
+	{ path: "/about", component: AboutView },
+];
+const router = createRouter({
+	history: createMemoryHistory(),
+	routes,
+});
 
+app.use(router);
 app.mount("#app");
